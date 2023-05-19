5e14c58d81751a68dffadb75881201a5520a4ec1
Hosein Abedi <hosein@Hoseins-MacBook-Pro.local>
2023-05-19 23:49:16 +0330

    bug fixed to run

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from'react';
 import { useDispatch } from 'react-redux';
 import { nanoid } from '@reduxjs/toolkit';
 import { postAdded } from './postSlice';
